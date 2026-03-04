3e65bcc06cc9b67dec22a5d749e1ce6d9b28797a
hystericca <viktor@hystericca.dev>
2026-03-04 00:57:57 -0800

    add embed thumbnail

--- src/utils/embed-utils.ts
+++ src/utils/embed-utils.ts
@@ -2,10 +2,14 @@ import { EmbedBuilder } from 'discord.js';
 
 const EMBED_COLOR = 0xffd1e1;
 const EMBED_FOOTER = '˖                        ۪                             ⊹';
+const EMBED_THUMBNAIL = 'https://cdn.discordapp.com/emojis/1439456764348334222.webp?animated=true';
 
 // creates an embed duh
 export function createEmbed(description?: string): EmbedBuilder {
-    const embed = new EmbedBuilder().setColor(EMBED_COLOR).setFooter({ text: EMBED_FOOTER });
+    const embed = new EmbedBuilder()
+        .setColor(EMBED_COLOR)
+        .setFooter({ text: EMBED_FOOTER })
+        .setThumbnail(EMBED_THUMBNAIL);
     if (description) embed.setDescription(description);
     return embed;
 }
